401b5606faf86ffac4a6b3fe51afe82afe4d9e8f
Yaz Khoury <yaz.khoury@gmail.com>
2019-05-24 17:25:56 -0400

    feat: Deploy working GKE terraform script

--- gcp/construct/main.tf
+++ gcp/construct/main.tf
@@ -4,25 +4,50 @@ provider "google" {
     zone = "us-central1-c"
 }
 
-resource "google_compute_instance" "vm_instance" {
-    name = "ether-instance"
-    machine_type = "n1-standard-1"
+resource "google_container_cluster" "primary" {
+    name = "ether-cluster"
+    
+    remove_default_node_pool = true
+    initial_node_count = 1
 
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-9"
-        }
-    }
-
-    network_interface {
-        network = "${google_compute_network.vpc_network.self_link}"
-        access_config {
-            
-        }
+    master_auth {
+        username = ""
+        password = ""
     }
 }
 
-resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
-    auto_create_subnetworks = "true"
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+    name = "my-node-pool"
+    cluster = "${google_container_cluster.primary.name}"
+    node_count = 3
+    
+    node_config {
+        preemptible = true
+        machine_type = "n1-standard-1"
+
+        metadata = {
+            disable-legacy-endpoints = "true"
+        }
+
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ]
+    }
+}
+
+output "client_certificate" {
+    value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+}
+
+output "client_key" {
+    value = "${google_container_cluster.primary.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+    value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+}
+
+resource "google_compute_address" "ip_address" {
+    name = "ethercluster-address"
 }
