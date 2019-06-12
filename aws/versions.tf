8783c1a54b69bb444c491be850266cee86f9bf46
Yaz Khoury <yaz.khoury@gmail.com>
2019-06-12 00:13:36 -0400

    feat: Setup remote state saving

--- aws/versions.tf
+++ aws/versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+    required_version = ">= 0.12"
+    backend "remote" {
+        # Check TF_CLI_CONFIG_FILE for credentials
+        hostname = "app.terraform.io"
+        organization = "ethercloud"
+        workspaces {
+            name = "workspace"
+        }
+    }
 }
