69159bc8fbc9e8e9d232d0aaf38f6e6c7806feb4
Yaz Khoury <yaz.khoury@gmail.com>
2019-06-11 23:05:03 -0400

    feat: Clean up aws secret, add instance

--- aws/ethercloud.tf
+++ aws/ethercloud.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+    region     = "us-east-1"
+}
+
+resource "aws_instance" "ethercloud" {
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
 }
